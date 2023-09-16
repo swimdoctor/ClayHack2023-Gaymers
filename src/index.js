7d7b4f8089aeb4cbb2494188c0e0f31b39bf1114
GarrettT <garrett59100@gmail.com>
2023-09-16 15:19:27 -0400

    gamer 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './modules/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./modules/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
