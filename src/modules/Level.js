4a9f924a3e8031a005700bc108939ca693e7cf9b
Dylan Shaffer <swimdoctor5@gmail.com>
2023-09-16 15:33:02 -0400

    ROTATION

--- src/modules/Level.js
+++ src/modules/Level.js
@@ -4,25 +4,29 @@ import { ReactP5Wrapper } from '@p5-wrapper/react';
 const Piece = {
     None:'None',
     Emitter:'Emitter',
-    Mirror:'https://raw.githubusercontent.com/swimdoctor/Lazal/0000eb7397cab0ae82133c1ca215e2afa3107700/public/Mirror_One_Side.svg',
+    Mirror:'https://raw.githubusercontent.com/swimdoctor/Lazal/0000eb7397cab0ae82133c1ca215e2afa3107700/public/Mirror_One_Side.svg', // M
+	Mirror2:'no', //N
+	Mirror3:'no2', //m
+	Mirror4:'no3', //n
     DoubleMirror:'DoubleMirror',
     Block:'Block',
     Prism:'Prism',
     TransMirror:'TransMirror',
-    Wall:'Wall'
+    Wall:'Wall',
+	Target:'Target'
 }
 
 let levels = {
     "9/16/2023": {
         "grid": [
-            'XXXXXXXX',
-            'XXXXXXXX',
-            'XXXXXXXX',
-            'XXXX XXX',
-            'XXXX XXX',
-            'XXXX XXX',
-            'XXXXMXXX',
-            'XXXXXXXX',
+            'XXEXXXXX',
+            'X      X',
+            'X M  m X',
+            'X      X',
+            'X  N n X',
+            'X      X',
+            'X      X',
+            'XXXTXXXX',
         ]
     }
 }
@@ -39,10 +43,20 @@ class Grid{
                 switch (char) {
                     case 'X':
                         output = Piece.Wall;
-                    break;
+                    	break;
                     case 'M':
                         output = Piece.Mirror;
-                    break;
+                    	break;
+					case 'N':
+						output = Piece.Mirror2;
+						break;
+					case 'm':
+						output = Piece.Mirror3;
+						break;
+					case 'n':
+						output = Piece.Mirror4;
+						break;
+
                 }
                 output_bracket.push(output);
             }
@@ -95,17 +109,46 @@ function sketch(p5) {
                 let tile_h = (600 - 8 - 4) / gameGrid.ySize;
 
                 let piece_type = gameGrid.grid[y][x];
+				
+				p5.fill(200);
+				p5.rect(106 + x * tile_w, 6 + y * tile_h, tile_w, tile_h);
                 switch (piece_type) {
                     case Piece.Wall:
                         p5.fill(150);
                         p5.rect(106 + x * tile_w, 6 + y * tile_h, tile_w, tile_h);
                     break;
                     case Piece.Mirror:
+						p5.push();
+						p5.translate(106 + x * tile_w + tile_w*.5, 6 + y * tile_h + tile_h*.5);
+						p5.rotate(0);
+						p5.translate(-(106 + x * tile_w + tile_w*.5), -(6 + y * tile_h + tile_h*.5));
                         p5.image(myImage, 106 + x * tile_w, 6 + y * tile_h, tile_w, tile_h);
+						p5.pop();
                     break;
-                    default:
-                        p5.fill(200);
-                        p5.rect(106 + x * tile_w, 6 + y * tile_h, tile_w, tile_h);
+                    case Piece.Mirror2:
+						p5.push();
+						p5.translate(106 + x * tile_w + tile_w*.5, 6 + y * tile_h + tile_h*.5);
+						p5.rotate(Math.PI/2);
+						p5.translate(-(106 + x * tile_w + tile_w*.5), -(6 + y * tile_h + tile_h*.5));
+                        p5.image(myImage, 106 + x * tile_w, 6 + y * tile_h, tile_w, tile_h);
+						p5.pop();
+                    break;
+                    case Piece.Mirror3:
+						p5.push();
+						p5.translate(106 + x * tile_w + tile_w*.5, 6 + y * tile_h + tile_h*.5);
+						p5.rotate(Math.PI);
+						p5.translate(-(106 + x * tile_w + tile_w*.5), -(6 + y * tile_h + tile_h*.5));
+                        p5.image(myImage, 106 + x * tile_w, 6 + y * tile_h, tile_w, tile_h);
+						p5.pop();
+                    break;
+                    case Piece.Mirror4:
+						p5.push();
+						p5.translate(106 + x * tile_w + tile_w*.5, 6 + y * tile_h + tile_h*.5);
+						p5.rotate(3*Math.PI/2);
+						p5.translate(-(106 + x * tile_w + tile_w*.5), -(6 + y * tile_h + tile_h*.5));
+                        p5.image(myImage, 106 + x * tile_w, 6 + y * tile_h, tile_w, tile_h);
+						p5.pop();
+                    break;
                 }
     		}
     	}
